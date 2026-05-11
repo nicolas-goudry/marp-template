06fc65ed6546e18caace706c345c9250f26ebf1a
Nicolas Goudry <goudry.nicolas@gmail.com>
2026-05-11 14:37:38 +0200

    chore: init flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Marpit Slides Template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ...
+    }@inputs:
+    let
+      inherit (inputs.nixpkgs) lib;
+
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forEachSupportedSystem =
+        f:
+        lib.genAttrs supportedSystems (
+          system:
+          f (
+            import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            }
+          )
+        );
+    in
+    { };
+}
